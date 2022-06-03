5dbc3be9f5a65c4f17c5067b9239659778a195c2
Joe <joe.attah1@icloud.com>
2022-06-03 00:29:12 -0500

    First Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import BookmarkAddTwoToneIcon from "@mui/icons-material/BookmarkAddTwoTone";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <BookmarkAddTwoToneIcon />
+        Bookmark App
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
